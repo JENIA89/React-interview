4f4ad728af5bc72d85cfd752b6dfd90bf062d0b2
Evgene Popolamov <popolamov908@gmail.com>
2020-11-29 23:15:00 +0300

    feat: add questions

---
+++ src/components/FinishedQuiz/FinishedQuiz.js
@@ -0,0 +1,8 @@
+import classes from "./FinishedQuiz.module.css";
+import React from "react";
+
+const FinishedQuiz = (props) => {
+  return <div className={classes.FinishedQuiz}>Finish</div>;
+};
+
+export default FinishedQuiz;
--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -1,17 +1,97 @@
 import classes from "./Quiz.module.css";
 import ActiveQuiz from "../../components/ActiveQuiz/ActiveQuiz";
 import React from "react";
+import FinishedQuiz from "../../components/FinishedQuiz/FinishedQuiz";
 
 class Quiz extends React.Component {
   state = {
-    quiz: [],
+    activeQuestion: 0,
+    isFinished: false,
+    answerState: null,
+    quiz: [
+      {
+        question: "Какого цвета небо?",
+        rightAnswer: 2,
+        id: 1,
+        answers: [
+          { text: "Черный", id: 1 },
+          { text: "Синий", id: 2 },
+          { text: "ВКрасный", id: 3 },
+          { text: "Зеленый", id: 4 },
+        ],
+      },
+      {
+        question: "В каком году основали Санкт-Петербург?",
+        rightAnswer: 3,
+        id: 2,
+        answers: [
+          { text: "1700", id: 1 },
+          { text: "1702", id: 2 },
+          { text: "1703", id: 3 },
+          { text: "1803", id: 4 },
+        ],
+      },
+    ],
   };
+
+  onAnswerClickHandler = (answerId) => {
+    if (this.state.answerState) {
+      const key = Object.keys(this.state.answerState)[0];
+      if (this.state.answerState[key] === "success") {
+        return;
+      }
+    }
+
+    const question = this.state.quiz[this.state.activeQuestion];
+
+    if (question.rightAnswer === answerId) {
+      this.setState({
+        answerState: { [answerId]: "success" },
+      });
+
+      const timer = window.setTimeout(() => {
+        if (this.isQuizFinished()) {
+          this.setState({
+            isFinished: true,
+          });
+        } else {
+          this.setState({
+            activeQuestion: this.state.activeQuestion + 1,
+            answerState: null,
+          });
+        }
+
+        window.clearTimeout(timer);
+      }, 1000);
+    } else {
+      this.setState({
+        answerState: { [answerId]: "error" },
+      });
+    }
+  };
+
+  isQuizFinished() {
+    return this.state.activeQuestion + 1 === this.state.quiz.length;
+  }
+
   render() {
     return (
       <div className={classes.Quiz}>
         <div className={classes.QuizWrapper}>
-          <h1>Quiz</h1>
-          <ActiveQuiz />
+          <h1>Ответьте на все вопросы</h1>
+
+          {this.state.isFinished ? (
+            <FinishedQuiz />
+          ) : (
+            <ActiveQuiz
+              answers={this.state.quiz[this.state.activeQuestion].answers}
+              question={this.state.quiz[this.state.activeQuestion].question}
+              onAnswerClick={this.onAnswerClickHandler}
+              quizLength={this.state.quiz.length}
+              answerNumber={this.state.activeQuestion + 1}
+              state={this.state.answerState}
+            />
+          )}
         </div>
       </div>
     );
